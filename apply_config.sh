94ae6c79fc25ddb2ab97b68d5357a18030ff3008
Olivier kessler <olivier.kessler@protonmail.com>
2023-04-08 11:28:03 +0200

    Update the config loading scripts

---
+++ apply_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r .config/nvim/* ~/.config/nvim/
+cp -r .config/i3/* ~/.config/i3
+cp .bash_profile ~
--- stage_current_config.sh
+++ stage_current_config.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 cp -r ~/.config/nvim/ .config/
 cp -r ~/.config/i3/ .config/
+cp ~/.bash_profile ./
