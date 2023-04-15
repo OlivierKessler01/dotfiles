015021e0f154da7181e512c8b25b9902a8febf3f
Olivier Kessler <olivier.kessler@bioserenity.com>
2023-04-15 18:33:26 +0200

    add mapping of esc to jk

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,6 +20,7 @@ vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap=true, silent=true})
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
