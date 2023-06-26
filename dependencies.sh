92aeb3717ecf4621ed813403b84027356500b440
Olivier Kessler <olivier.kessler@bioserenity.com>
2023-06-26 18:38:06 +0200

    Add LSP to my neovim

--- dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo dnf install \
-    i3 redshift redshift-gtk alacritty nitrogen picom
--- requirements_dnf.sh
+++ requirements_dnf.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/bash
 
-sudo dnf update && sudo dnf install maim xclip redshift \
-i3 pavucontrol emacs ripgrep picom
+sudo dnf update 
+sudo dnf install maim xclip redshift redshift-gtk \
+i3 pavucontrol emacs ripgrep picom nitrogen g++
 
 #Vim leetcode
 pip3 install pynvim keyring browser-cookie3
+
+#Install node
+wget https://deb.nodesource.com/setup_19.x -O node.sh
+chmod +x node.sh
+/bin/bash node.sh
+rm node.sh
+
+
+# Install Python LSP client
+npm i -g pyright
