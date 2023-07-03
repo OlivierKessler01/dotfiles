235e05f0fee6694dee54dc75e03327bcdc9bf56d
Olivier Kessler <olivier.kessler@bioserenity.com>
2023-07-03 10:39:50 +0200

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,7 +98,7 @@ return require('packer').startup(function(use)
             { 'hrsh7th/nvim-cmp' }, -- Required
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
             { 'L3MON4D3/LuaSnip' }, -- Required
-        }
+        },
         run = function () 
             -- LSP Config
             local lsp = require('lsp-zero').preset({})
