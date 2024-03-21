982ad24202e1aaf65e93360ea13fd071f4d44dc9
Olivier Kessler <olivier.kessler@bioserenity.com>
2024-03-21 15:22:39 +0100

    Fix the LSP config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,18 +4,14 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
+lsp.ensure_installed({
     'tsserver',
     'eslint',
     'pyright',
     'clangd'
-  },
-  handlers = {
-    lsp.default_setup,
-  },
 })
+
+lsp.setup()
 
 
 
