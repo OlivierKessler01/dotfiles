70db929253bf7f5caae0ee6d38a463042dbcd975
Olivier Kessler <olivier.kessler@bioserenity.com>
2024-03-28 14:51:48 +0100

    feat(Nvim): Add which-key and gitsigns plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -143,5 +143,32 @@ return require('packer').startup(function(use)
         },
     })
 
+  -- which-key helps you remember key bindings by showing a popup
+  -- with the active keybindings of the command you started typing.
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        require("which-key").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
+    
+    -- gitsigns shows symbols for updated lines
+    use {'lewis6991/gitsigns.nvim'}
+    require('gitsigns').setup {
+        signs = {
+            add          = { text = '+' },
+            change       = { text = '│' },
+            delete       = { text = '_' },
+            topdelete    = { text = '‾' },
+            changedelete = { text = '~' },
+            untracked    = { text = '┆' },
+        },
+    }
 end)
 
