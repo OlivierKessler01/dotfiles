1008f7ac7615082e307e555f17c7227050254dce
Olivier Kessler <olivier.kessler@protonmail.com>
2024-05-30 14:35:34 +0200

    Some cleaning

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #
-rm ~/.config/nvim && true
+rm -r ~/.config/nvim && true
 ln -s $(pwd)/.config/nvim ~/.config
 
-rm ~/.config/i3 && true
+rm -r ~/.config/i3 && true
 ln -s $(pwd)/.config/i3 ~/.config
 
-rm ~/.config/alacritty && true
-ln -s $(pwd)/.config/alacritty ~/.config
+rm -r ~/.config/alacritty && true
+ln -s $(pwd)/.config/alacritty ~/.config/
 
 rm ~/.tmux.conf && true
 ln -s $(pwd)/.tmux.conf ~
