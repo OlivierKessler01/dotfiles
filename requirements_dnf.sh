2b521c86f667e04d38c98e16086a66ef514f8039
Olivier Kessler <olivier.kessler@protonmail.com>
2024-08-05 12:24:12 +0200

    Update the list of dependencies

--- requirements_dnf.sh
+++ requirements_dnf.sh
@@ -5,14 +5,17 @@ sudo dnf update
 sudo dnf install maim xclip redshift redshift-gtk \
 i3 pavucontrol ripgrep picom nitrogen g++ git alacritty nvim \
 xrandr autorandr
-
-#Install node
-wget https://rpm.nodesource.com/setup_19.x -O node.sh
+## Needed to build kernel modules (like lttng)
+sudo dnf install kernel-devel
+## Tracing
+sudo dnf install lttng-ust lttng-tools
+## nodeJS
+wget https://rpm.nodesource.com/setup_20.x -O node.sh
 chmod +x node.sh
 /bin/bash node.sh
 rm node.sh
 
-#Set-up the timezone
+# Set-up the timezone
 timedatectl set-timezone Europe/Paris
 
 
