9aca4c18e76db85f0adbac1627b9e3d311f61262
Olivier Kessler <olivier.kessler@protonmail.com>
2024-08-05 12:18:05 +0200

    Update the dependency list and bootstrap the timezone setting

--- requirements_dnf.sh
+++ requirements_dnf.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/bash
 
+# Install common software I use
 sudo dnf update 
 sudo dnf install maim xclip redshift redshift-gtk \
-i3 pavucontrol ripgrep picom nitrogen g++ git
+i3 pavucontrol ripgrep picom nitrogen g++ git alacritty nvim \
+xrandr autorandr
 
 #Install node
 wget https://rpm.nodesource.com/setup_19.x -O node.sh
 chmod +x node.sh
 /bin/bash node.sh
 rm node.sh
+
+#Set-up the timezone
+timedatectl set-timezone Europe/Paris
 
 
