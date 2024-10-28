a5f46abad1d54f7bda6d64ef99f5ae1ca178aa89
Olivier Kessler <olivier.kessler@protonmail.com>
2024-10-28 17:36:05 +0100

    Add some more config to the lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,3 +1,9 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
